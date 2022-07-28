3d3811f94304bdfdd07da94547da82058f86ad92
SSergiyV <ssergeyval@gmail.com>
2022-07-28 22:52:27 +0300

    final

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
-    phpinfo();
+require_once 'classes/ValueObject.php';
 
-    $pdo = new PDO("pgsql:host=pgsql;dbname=postgres", "postgres", "root");
+try {
+    $color = new ValueObject(200, 200, 200);
+    $color_2 = $color -> mix (new ValueObject(100, 100, 100));
+    var_dump($color -> equals($color, $color_2));
+    $rgb = ValueObject::random();
+    var_dump($rgb);
+    echo $color -> getRed();
+    echo $color -> getGreen();
+    echo $color -> getBlue();
+}
+catch (Exception $e) {
+    echo $e -> getMessage();
+}
