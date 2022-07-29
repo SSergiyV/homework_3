d3e78e456b9407abdabe411401fc36acaef531b6
SSergiyV <ssergeyval@gmail.com>
2022-07-29 18:19:14 +0300

    Homework_5 Exception

---
+++ classes/User.php
@@ -0,0 +1,44 @@
+<?php
+
+class UserException extends Exception {}
+
+class User {
+
+    public static int $id = 0;
+    private string $password;
+    private string $email;
+
+    /**
+     * @throws Exception
+     */
+    public function __construct($password, $email) {
+        self::$id++;
+        $this -> setPassword($password);
+        $this -> setEmail($email);
+    }
+
+    public function getUserData(): string {
+        return "<br>ID: " . self::$id . "<br><br>Email: {$this -> email}<br>";
+    }
+
+    /**
+     * @param string $password
+     * @throws Exception
+     */
+    public function setPassword(string $password): void
+    {
+        if (strlen($password) > 8) {
+            throw new Exception("<br>Invalid value<br><br>");
+        }
+        $this->password = $password;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+}
--- index.php
+++ index.php
@@ -1,17 +1,36 @@
 <?php
 
+error_reporting(-1);
+
 require_once 'classes/ValueObject.php';
+require_once 'classes/User.php';
+
+function debug ($data) {
+    echo '<pre>' . print_r($data, 1) . '<pre>';
+}
 
 try {
     $color = new ValueObject(200, 200, 200);
-    $color_2 = $color -> mix (new ValueObject(100, 100, 100));
-    var_dump($color -> equals($color, $color_2));
+    $color_1 = new ValueObject(200, 200, 200);
+    $color_2 = $color -> mix (new ValueObject(200, 200, 200));
+    var_dump($color -> equals($color, $color_1));
+    echo "<br>";
     $rgb = ValueObject::random();
-    var_dump($rgb);
+    debug($rgb);
+    echo "<br>";
     echo $color -> getRed();
+    echo "<br>";
     echo $color -> getGreen();
+    echo "<br>";
     echo $color -> getBlue();
+    echo "<br>";
+    $user = new User(2545524, "gft@jhn.ki");
+    echo $user -> getUserData();
 }
-catch (Exception $e) {
-    echo $e -> getMessage();
+catch (Exception $exception) {
+    echo $exception -> getMessage();
+    echo "File: " . $exception -> getFile();
+    echo "<br><br>";
+    echo "Line: " . $exception -> getLine();
 }
+
